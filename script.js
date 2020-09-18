bfb193d703cb667f40060307bc858592d36a1837
Igor <shkatulovigor@gmail.com>
2020-09-18 11:59:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+console.log('Hello world!');
+
+//alert("some code");
+
+let number = 5;
+const lefBorderWidth = 1;
+
+console.log(number);
+
+const obj = {
+    a: 50    
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+const objMan = {
+    name: "John",
+    age: 25,
+    isMarried: false   
+};
+
+console.log(objMan.name);
+console.log(objMan["age"]);
+*/
+
+const answers = [];
+
+answers[0] = prompt("Как ваше имя?","");
+answers[1] = prompt("Какая у вас фамилия?","");
+answers[2] = prompt("Сколько вам лет?","");
+
+document.write(answers);
